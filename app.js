4c86bcc5c8257941952547f3e42bfeddbb7ff1ff
Murat Kocak <muradkocak1998@gmail.com>
2022-06-29 11:21:47 +0300

    views partials esj kısımları olusturuldu

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
 
+//VİEW ENGİNE
+app.set('view engine', 'ejs');
+
+
+//MİDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-    const blog = { 
-        id: 1, title: "Blog title", 
-        description: "Blog description" 
-    }
-    res.send(blog)
-})
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/add', (req, res) => {
+  res.render('post');
+});
 const port = 3000;
 
-app.listen(3000,()=>{
-    console.log(`sunucu ${port} porunda calisaktadir..`)
-})
+app.listen(3000, () => {
+  console.log(`sunucu ${port} porunda calisaktadir..`);
+});
